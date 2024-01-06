203f0c21beba453684805f1822641a24dedea707
unknown <riya pc>
2024-01-06 11:41:18 +0530

    update

--- provider/urls.py
+++ provider/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
 from .import views
+from provider.views import servicesmain
+
 
 urlpatterns = [
     
     path('', views.index),
+    path('',servicesmain,name="service"),
 ]
--- provider/views.py
+++ provider/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
+def servicesmain(request):
+    return render(request,"servicesmain.html",context={})
 # Create your views here.
