b6423b5a9683ed885aac3006dd4f53ef9ad5305d
unknown <riya pc>
2024-01-12 13:49:38 +0530

    update

--- static/js/custom.js
+++ static/js/custom.js
@@ -35,7 +35,7 @@ $(".owl-carousel").owlCarousel({
 
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(18.407157072276252, 73.50305509471461),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
