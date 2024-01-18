5b4a46e51c1c3e7759afa8546b85b8e69f6dfcad
Abhidev SP <abhidev.sp@msds.christuniversity.in>
2024-01-18 14:19:47 +0530

    update

--- clickadmin/urls.py
+++ clickadmin/urls.py
@@ -10,17 +10,7 @@ urlpatterns = [
     path('about',about,name="about"),
     path('contact',contact,name="contact"),
     path('service',service,name="service"),
-<<<<<<< HEAD
-   
-=======
-<<<<<<< HEAD
-    path('login',login,name="login")
-
-]
-=======
     path('login',login,name="login"),
->>>>>>> 5957d7d3a8c0223b75313418843fd803ce754d0e
 ]
-    
-   
->>>>>>> 3f26a95bf680c7180a03b4fc30c19c19c369e106
+
+
