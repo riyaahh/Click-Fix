3f26a95bf680c7180a03b4fc30c19c19c369e106
Abhidev SP <abhidev.sp@msds.christuniversity.in>
2024-01-18 13:36:29 +0530

    update

--- clickadmin/urls.py
+++ clickadmin/urls.py
@@ -4,19 +4,13 @@ from .views import index,about,contact,service,login
 
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('',index,name="dashboard"),
     path('index',index,name="index"),
     path('about',about,name="about"),
     path('contact',contact,name="contact"),
     path('service',service,name="service"),
-    path('login',login,name="login")
-=======
-    
-    path('',views.index,name="index"),
-    path('about',views.about,name="about"),
-    path('contact',views.contact,name="contact"),
-    path('service',views.service,name="service"),
->>>>>>> 0b728caec7e4944172b3924072923e6a48826768
-    
+    path('login',login,name="login"),
 ]
+    
+   
