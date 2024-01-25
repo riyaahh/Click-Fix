830d03e9b836421875ccd78ac57cd64791a14ed9
Abhidev SP <abhidev.sp@msds.christuniversity.in>
2024-01-25 09:24:39 +0530

    update

--- customer/views.py
+++ customer/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.contrib.auth.forms import UserCreationForm
+
+from .models import *
+# from .forms import OrderForm
+# from .filters import OrderFilter
 
 
 def index(request):
     return render(request,"provider/index.html",context={})
 
 def register(request):
-    return render(request, 'customer\\register.html')
+    
+    context={}
+
+    return render(request, 'customer\\register.html',context)
 
 def user(request):
     return render(request,'customer\\user.html')
 
 def login(request): 
-    return render(request, 'customer\\login.html')
+    context={}
+    return render(request, 'customer\\login.html',context)
     # if request.method== 'POST':
     #     username=request.POST('email')
     #     password=request.POST('password')
