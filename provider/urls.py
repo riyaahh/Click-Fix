c1c8e4b89dadc934855f2fe2905061bcf13acec5
unknown <riya pc>
2024-01-26 20:06:26 +0530

    update

--- provider/urls.py
+++ provider/urls.py
@@ -6,4 +6,6 @@ from .import views
 urlpatterns = [
       path('bookings',views.bookings,name="bookings"),
       path('bookform',views.bookform,name="bookform"),
+      path('prof',views.prof,name="prof"),
+      path('profform',views.profform,name="profform")
 ]
--- provider/views.py
+++ provider/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from provider.models import 
 
 
 # def index(request):
@@ -15,3 +16,7 @@ def bookings(request):
     return render(request,"provider/bookings.html",context={})
 def bookform(request):
     return render(request,"provider/bookform.html",context={})
+def prof(request):
+    return render(request,"provider/prof.html",context={})
+def profform(request):
+    return render(request,"provider/profform.html",context={})
