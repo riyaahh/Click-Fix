1e82f974789500441358a2d95355b187afce1e53
unknown <riya pc>
2024-01-26 13:31:43 +0530

    update

--- clickadmin/urls.py
+++ clickadmin/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 
 from clickadmin import views
-from clickadmin.views import index,about,contact,service,login
+from clickadmin.views import index,about,contact,service
 
 from .import views
 from . views import index,about,contact,service
@@ -16,9 +16,9 @@ urlpatterns = [
     path('service', views.service,name="service"),
     # path('login', views.login ,name="login"),
 
-    path('about/', views.about ,name="about"),
-    path('contact/', views.contact ,name="contact"),
-    path('service/', views.service,name="service"),
+    # path('about/', views.about ,name="about"),
+    # path('contact/', views.contact ,name="contact"),
+    # path('service/', views.service,name="service"),
 
     # path('register' , views.register, name='register'),
     # path('user',views.user,name="user")
